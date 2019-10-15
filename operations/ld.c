5e759d68e035df4d4eae9afb1835749b57f04a9f
Jim Heeresma <jheeresm@f1r5s11.codam.nl>
2019-10-15 15:45:03 +0200

    added ld

--- operations/ld.c
+++ operations/ld.c
@@ -11,3 +11,20 @@
 /* ************************************************************************** */
 
 #include "../corewar.h"
+
+void		ld_op(t_vm *vm, t_cursor *cursor)
+{
+	int value;
+
+
+	if ((char)cursor->opcode & 0b01000000 > 0)
+		value = vm->memory[cursor->position +
+		modulo(cursor->operation.arg[0], IDX_MOD)];
+	else
+		value = cursor->reg[cursor->operation.arg[0]];
+	cursor->reg[cursor->operation.arg[1]] = value;
+	if (value == 0)
+		cursor->carry = 1;
+	else
+		cursor->carry = 0;
+}
