c27ccd42bb8406ea4c82a340c31425ae94744e75
Anna Yundina <ayundina@f1r6s10.codam.nl>
2019-10-15 16:42:15 +0200

    safe comparison to R1

--- operations/live.c
+++ operations/live.c
@@ -20,17 +20,26 @@
 ** 1. Updates when cursor last reported alive.
 ** 2. If cursor's r1 == Argument1, set player and cursor as last reported alive.
 */
+
 void    live(t_vm *vm, t_cursor *cursor)
 {
-   if (cursor->operation.arg[0] == cursor->reg[1])
-   {
-       //champ_reported alive
-       vm->game.last_alive_champ == cursor->reg[1];//r1 == cursor->reg[1]?
-   }
-   //cursor reported alive in current cycle
-   cursor->last_live = vm->game.cycles_to_die;
-   //number of lives reported increased
-   vm->game.nbr_live++;
-   //at the end of each cycle nmbr_live will be reset to zero
-   //cursor reported alive
+    int i;
+
+    i = 0;
+    while (i < 4)
+    {
+        if (cursor->operation.arg[0] == -vm->champions[i].id)//-?(?:AYU)
+        {
+            //champ_reported alive
+            vm->game.last_alive_champ = vm->champions[i].id;
+            vm->champions[i].last_live = vm->game.cycles_to_die;//do we need it?(?:AYU)
+        }
+        i++;
+    }
+    //cursor reported alive in current cycle
+    cursor->last_live = vm->game.cycles_to_die;
+    //cursor->live++;//at the end of the cycle if (cursor->live == 0){delete cursor} (?:AYU)
+    //number of lives reported increased
+    vm->game.nbr_live++;
+    //at the end of each cycle nmbr_live will be reset to zero
 }
