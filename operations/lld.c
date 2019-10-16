b5a48abd7ed004ea3dbeaa7a8b346e78a282bec4
Tim De jong <tide-jon@f0r1s12.codam.nl>
2019-10-16 20:12:57 +0200

    all operations done except for live

--- operations/lld.c
+++ operations/lld.c
@@ -1,13 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   longload.c                         	            :+:    :+:            */
+/*   lld.c                                              :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: tmeulenb <marvin@codam.nl>                   +#+                     */
+/*   By: tide-jon <tide-jon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/07/21 12:25:51 by jheeresm      #+#    #+#                 */
-/*   Updated: 2019/07/21 12:25:52 by jheeresm      ########   odam.nl         */
+/*   Created: 2019/10/16 19:50:59 by tide-jon       #+#    #+#                */
+/*   Updated: 2019/10/16 20:12:49 by tide-jon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../corewar.h"
+
+void		lld(t_vm *vm, t_cursor *cursor)
+{
+	int value;
+
+	value = cursor->operation.arg[0];
+	cursor->reg[cursor->operation.arg[1] - 1] = value;
+	if (value == 0)
+		cursor->carry = 1;
+	else
+		cursor->carry = 0;
+}
