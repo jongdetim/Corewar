d104557087602e927f6ab5bc92b3b6824dd43193
ayundina <ayundina@f0r4s19.codam.nl>
2019-10-17 17:02:13 +0200

    clened up

--- operations/live.c
+++ operations/live.c
@@ -21,7 +21,6 @@
 ** 2. If cursor's r1 == Argument1, set player and cursor as last reported alive.
 */
 
-<<<<<<< HEAD
 /*
 **	wasn't tested!
 **
@@ -37,18 +36,12 @@
 */
 
 void    live_op(t_vm *vm, t_cursor *cursor)
-=======
-//wasn't tested!
-
-void    live(t_vm *vm, t_cursor *cursor)
->>>>>>> 458ad14c041df0c13c2157a2be5de84eea3858f5
 {
     int i;
 
 	i = 0;
 	while (i < 4)
 	{
-<<<<<<< HEAD
 		if (cursor->operation.arg[0] == vm->champions[i].id)
 		{
 			vm->game.last_alive_champ = vm->champions[i].id;
@@ -57,20 +50,4 @@ void    live(t_vm *vm, t_cursor *cursor)
 	}
 	cursor->last_live = vm->game.cycles;
 	vm->game.nbr_live++;
-=======
-		if (cursor->operation.arg[0] == vm->champions[i].id)//-?(?:AYU)
-		{
-			//champ_reported alive
-			vm->game.last_alive_champ = vm->champions[i].id;
-			vm->champions[i].last_live = vm->game.cycles_to_die;//do we need it?(?:AYU)
-		}
-		i++;
-	}
-	//cursor reported alive in current cycle
-	cursor->last_live = vm->game.cycles_to_die;
-	//cursor->live++;//at the end of the cycle if (cursor->live == 0){delete cursor} (?:AYU)
-	//number of lives reported increased
-	vm->game.nbr_live++;
-	//at the end of each cycle nmbr_live will be reset to zero
->>>>>>> 458ad14c041df0c13c2157a2be5de84eea3858f5
 }
