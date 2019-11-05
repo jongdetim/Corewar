fa4fe4c9ea93455fd284e53a4ea35b72200cc115
Tim de Jong <tide-jon@codam.nl>
2019-11-05 21:17:04 +0100

    minor updates to visualizer; added color and player comments/lives

--- virtual_machine/srcs/visualizer.c
+++ virtual_machine/srcs/visualizer.c
@@ -40,16 +40,59 @@ static void		fill_game_variables(WINDOW *game_variables, t_vm *vm)
 	mvwprintw(game_variables, 2, 4, "total cycles:");
 	mvwprintw(game_variables, 4, 4, "cycles to die:");
 
+	start_color();
+	
 	while (i < vm->champion_count)
 	{
-		mvwprintw(game_variables, 7 + i * 4, 4, "player");
-		mvwprintw(game_variables, 7 + i * 4, 11, ft_itoa(vm->champions[i].id));
-		mvwprintw(game_variables, 7 + i * 4, 17, vm->champions[i].name);
-		mvwprintw(game_variables, 8 + i * 4, 4, vm->champions[i].comment);
+		init_pair(i + 1, i + 1, COLOR_BLACK);
+		wattron(game_variables, COLOR_PAIR(i + 1));
+		mvwprintw(game_variables, 7 + i * 7, 4, "player");
+		mvwprintw(game_variables, 7 + i * 7, 11, ft_itoa(vm->champions[i].id));
+		wattroff(game_variables, COLOR_PAIR(i + 1));
+		mvwprintw(game_variables, 7 + i * 7, 17, vm->champions[i].name);
+		wattron(game_variables, A_ITALIC);
+		mvwprintw(game_variables, 8 + i * 7, 4, "\"");
+		wprintw(game_variables, vm->champions[i].comment);
+		wprintw(game_variables, "\"");
+		wattroff(game_variables, A_ITALIC);
+		mvwprintw(game_variables, 12 + i * 7, 4, "last live:");
 		i++;
 	}
-
 	
+}
+
+void	update_game_variables(WINDOW *game_variables, t_vm *vm)
+{
+	int	i;
+
+	i = 0;
+	while (i < vm->champion_count)
+	{
+		mvwprintw(game_variables, 12 + i * 7, 24, ft_itoa(vm->champions[i].last_live));
+		i++;
+	}
+}
+
+void	update_memory(WINDOW *memory_display, t_vm *vm)
+{
+	int	i;
+	int	y;
+	int	x;
+
+	i = 0;
+	y = 1;
+	while (y < 65)
+	{
+		x = 2;
+		while (x < 194)
+		{
+			mvwprintw(memory_display, y, x, "%.2x ", vm->memory[i]);
+			x += 3;
+			i++;
+		}
+		y++;
+	}
+	wrefresh(memory_display);
 }
 
 void			visualizer(t_vm *vm)
@@ -57,15 +100,15 @@ void			visualizer(t_vm *vm)
 	static WINDOW	*memory_display;
 	static WINDOW	*game_variables;
 	static WINDOW	*corewar_header;
-	int				i;
 
 	iwait();
 
 	if (memory_display == NULL)
 	{
 		initscr();
-		keypad(stdscr, TRUE);
-		printw("Press F1 to exit");
+		cbreak();
+		noecho();
+		curs_set(0);
 
 		memory_display = newwin(66, 195, 0, 0);
 		corewar_header = newwin(10, 50, 0, 196);
@@ -82,20 +125,8 @@ void			visualizer(t_vm *vm)
 		wrefresh(game_variables);
 	}
 
-	i = 0;
-	int y = 1;
-	while (y < 65)
-	{
-		int x = 2;
-		while (x < 194)
-		{
-			mvwprintw(memory_display, y, x, "%.2x ", vm->memory[i]);
-			x += 3;
-			i++;
-		}
-		y++;
-	}
-	wrefresh(memory_display);
+	update_memory(memory_display, vm);
+	update_game_variables(game_variables, vm);
 
 	mvwprintw(game_variables, 2, 24, ft_itoa(vm->game.cycles));
 	mvwprintw(game_variables, 4, 24, ft_itoa(vm->game.cycles_to_die));
