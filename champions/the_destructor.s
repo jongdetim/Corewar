07f927d4d8f5a291347e33e71259c753eca98dee
Anna <anna@MacBook-Pro-van-jasper.local>
2019-11-12 22:56:15 +0100

    Our champions

---
+++ champions/the_destructor.s
@@ -0,0 +1,14 @@
+.name		"the destructor"
+.comment	"A lesson of how to be a man"
+
+fork_it:
+	fork	%:clone_it
+
+live_it:
+	st		r1, 6
+	live	%42
+
+clone_it:
+	ld		%1, r2 #load live opcode to r2
+	st		r2, 64 #set live opcode after 64 bytes
+	st		r1, 63 #set player id after live opcode
